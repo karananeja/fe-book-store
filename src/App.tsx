471d64bb2d736e668bfdf255dcc4c91b7fd445ae
karananeja <karananeja.r@gmail.com>
2024-01-07 22:25:40 +0530

    wrapped the app with queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { AppRoutes } from './routes';
+import { queryClient } from './services/queryClient';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
