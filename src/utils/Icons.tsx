731ddf9f359fe47309f2ac332fc3e9097d7fe717
karananeja <karananeja.r@gmail.com>
2024-01-08 03:12:39 +0530

    added few more icons

--- src/utils/Icons.tsx
+++ src/utils/Icons.tsx
@@ -129,4 +129,38 @@ export const Icons = {
       <path d='M16 12h2' />
     </svg>
   ),
+  close: (props: IconProps) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      {...props}
+    >
+      <path d='M18 6 6 18' />
+      <path d='m6 6 12 12' />
+    </svg>
+  ),
+  eyeOpen: (props: IconProps) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      {...props}
+    >
+      <path d='M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z' />
+      <circle cx='12' cy='12' r='3' />
+    </svg>
+  ),
 };
