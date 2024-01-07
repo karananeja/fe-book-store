58d4b085aa082c3a95076ac5b701fc103fc7b49d
karananeja <karananeja.r@gmail.com>
2024-01-08 00:16:21 +0530

    used react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppRoutes } from './routes';
 import { queryClient } from './services/queryClient';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AppRoutes />
+      <ReactQueryDevtools buttonPosition={'bottom-left'} />
     </QueryClientProvider>
   );
 }
