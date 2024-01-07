a84efe889b7c431800ee6eb7aab749b79d6a1917
karananeja <karananeja.r@gmail.com>
2024-01-08 03:13:11 +0530

    added few more types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -29,3 +29,5 @@ export type BooksTablePropsType = { bookList?: GetBookInfoType[] };
 export type BooksCardPropsType = BooksTablePropsType;
 
 export type BookCardPropsType = { book: GetBookInfoType };
+
+export type BookModalPropsType = BookCardPropsType & { onClose: () => void };
